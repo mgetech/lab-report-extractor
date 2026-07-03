f7171f6265b33a6f9e27ca1b634648acfd21d556
Majid G.E <mgetech@yahoo.com>
2026-07-03 23:14:59 +0200

    chore: project scaffold and config

---
+++ src/api/Program.cs
@@ -0,0 +1,8 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/health", () => Results.Ok(new { status = "healthy" }))
+    .WithName("GetHealth");
+
+app.Run();
